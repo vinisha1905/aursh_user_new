d06c2f1bdaab4eaeeed1c2b9b2caf32469029622
milin2311 <vinisha.oceanbit@gmail.com>
2026-05-21 10:57:51 +0530

    add git ignore

--- lib/utils/text_style.dart
+++ lib/utils/text_style.dart
@@ -2,27 +2,48 @@ import 'package:flutter/material.dart';
 
 import '../domain/constants.dart';
 
-TextStyle get style20600Secondary => TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: secondaryColor);
-TextStyle get style12400Secondary => TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: secondaryColor);
-TextStyle get style12400White => TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: white);
-TextStyle get style12500White => TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: white);
-TextStyle get style12500Primary => TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: primaryGreenColor);
-TextStyle get style14600Secondary => TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: secondaryColor);
-TextStyle get style15600Secondary => TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: secondaryColor);
-TextStyle get style12400Grey500 => TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: grey500Color);
-TextStyle get style11400Grey500 => TextStyle(fontSize: 11, fontWeight: FontWeight.w400, color: grey500Color);
-TextStyle get style15600PrimaryGreen => TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: primaryGreenColor);
-TextStyle get style13400White => TextStyle(fontSize: 13, color: Colors.white, fontWeight: FontWeight.w400);
-TextStyle get style16500Secondary => TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: secondaryColor);
-TextStyle get style16600Secondary => TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: secondaryColor);
-TextStyle get style16500PrimaryDark => TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: primaryGreenDarkColor);
-TextStyle get style14500White => TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: white);
-TextStyle get style14500Primary => TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: primaryGreenColor);
-TextStyle get style14400Grey600 => TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: grey600Color);
+TextStyle get style20600Secondary =>
+    TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: secondaryColor);
+TextStyle get style12400Secondary =>
+    TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: secondaryColor);
+TextStyle get style12400White =>
+    TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: white);
+TextStyle get style12500White =>
+    TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: white);
+TextStyle get style12500Primary => TextStyle(
+    fontSize: 12, fontWeight: FontWeight.w500, color: primaryGreenColor);
+TextStyle get style14600Secondary =>
+    TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: secondaryColor);
+TextStyle get style15600Secondary =>
+    TextStyle(fontSize: 15, fontWeight: FontWeight.w600, color: secondaryColor);
+TextStyle get style12400Grey500 =>
+    TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: grey500Color);
+TextStyle get style11400Grey500 =>
+    TextStyle(fontSize: 11, fontWeight: FontWeight.w400, color: grey500Color);
+TextStyle get style15600PrimaryGreen => TextStyle(
+    fontSize: 15, fontWeight: FontWeight.w600, color: primaryGreenColor);
+TextStyle get style13400White =>
+    TextStyle(fontSize: 13, color: Colors.white, fontWeight: FontWeight.w400);
+TextStyle get style16500Secondary =>
+    TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: secondaryColor);
+TextStyle get style16600Secondary =>
+    TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: secondaryColor);
+TextStyle get style16500PrimaryDark => TextStyle(
+    fontSize: 16, fontWeight: FontWeight.w500, color: primaryGreenDarkColor);
+TextStyle get style14500White =>
+    TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: white);
+TextStyle get style14500Primary => TextStyle(
+    fontSize: 14, fontWeight: FontWeight.w500, color: primaryGreenColor);
+TextStyle get style14400Grey600 =>
+    TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: grey600Color);
 
 // TextFiled
 
-TextStyle get textFiledTextStyle => TextStyle(color: secondaryColor, fontSize: 14, fontWeight: FontWeight.w400);
-TextStyle get disableTextFiledTextStyle => TextStyle(color: secondaryColor, fontSize: 14, fontWeight: FontWeight.w400);
-TextStyle get hintTextFiledTextStyle => TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: grey500Color);
-TextStyle get labelTextFiledTextStyle => TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: grey500Color);
+TextStyle get textFiledTextStyle =>
+    TextStyle(color: secondaryColor, fontSize: 14, fontWeight: FontWeight.w400);
+TextStyle get disableTextFiledTextStyle =>
+    TextStyle(color: secondaryColor, fontSize: 14, fontWeight: FontWeight.w400);
+TextStyle get hintTextFiledTextStyle =>
+    TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: grey500Color);
+TextStyle get labelTextFiledTextStyle =>
+    TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: grey500Color);
